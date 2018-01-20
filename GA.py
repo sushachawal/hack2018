885aaf0c1022c1f38183efe4868de5bd7cc95edd
sushachawal <sushachawal@gmail.com>
2018-01-20 20:51:32 +0000

    Add basic evolve

--- GA.py
+++ GA.py
@@ -1,9 +1,13 @@
 # import numpy as np
 import random
-
+import numpy as np
 
 max_power = 500
 min_power = 0
+percentage_good = 25
+
+def score(x):
+    return max(x)
 
 def create_population(size):
 
@@ -11,16 +15,19 @@ def create_population(size):
 
     for i in range(0, size):
         powers = []
-        for i in range(0,7):
+        for j in range(0,7):
             powers.append(random.randint(min_power, max_power))
 
         pop.append(powers)
 
+
     return pop
 
+population = create_population(5)
 
+graded = [(score(member), member) for member in population]
 
+graded = [x[1] for x in sorted(graded, key=lambda x:x[0], reverse = True)]
 
-def calculate_time(powers):
-
-
+print population
+print graded
