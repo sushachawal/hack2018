8fd608cdc104cac2d066abbb0e7ca3b8d79e7b05
sushachawal <sushachawal@gmail.com>
2018-01-20 22:36:53 +0000

    Add least squares method

---
+++ leastsquares.py
@@ -0,0 +1,7 @@
+def leastsquares(pop):
+    y[]
+    for x in range(0,10, 0.1):
+        y.append(pop.m * x + pop.c)
+    for i in range(0,len(y)-1)
+        score += abs(y[i]-data[i])
+    return score
