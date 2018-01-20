a9ed37fadc4fe972ac414e6aef9b07659b09f370
sushachawal <sushachawal@gmail.com>
2018-01-20 23:58:09 +0000

    Fix simulation syntax errors, combine hackathon in simulation

--- hackathon.py
+++ hackathon.py
@@ -1,21 +1,11 @@
-import stravalib
-from stravalib import Client
+import simulation
 
 
-STORED_ACCESS_TOKEN = "903910abaf69b186957a22c1227d6f19237cd233"
+section_power = [400,400,400,400,400,400,400]
 
-client = Client(access_token=STORED_ACCESS_TOKEN)
-athlete_data = client.get_athlete()
-activity_id = 887327831
-activity = client.get_activity(887327831)
+print simulation.get_time(section_power)
 
-types = ['time', 'altitude', 'velocity_smooth', 'distance', 'grade_smooth', 'watts']
-streams = client.get_activity_streams(activity_id, types = types, resolution='medium')
 
-# for i in range(0, len(streams['time'].data)):
-#     print "{} {}".format(streams['distance'].data[i], streams['watts'].data[i])
-
-print streams.keys()
 
 # if stream in streams.keys():
 #     print(stream)
--- simulation.py
+++ simulation.py
@@ -1,47 +1,57 @@
 import math
-import numpy
+import numpy as np
+import stravalib
+from stravalib import Client
 
-velocities = []
-y = []
-x = []
-inclin = []
+STORED_ACCESS_TOKEN = "903910abaf69b186957a22c1227d6f19237cd233"
 
-mass = 90
-g = 9.81
+client = Client(access_token=STORED_ACCESS_TOKEN)
+athlete_data = client.get_athlete()
+activity_id = 887327831
+activity = client.get_activity(887327831)
 
-area = 0.32
-Cd = 0.88
-A = 0.5 * 1.225 * area * Cd
+types = ['time', 'altitude', 'velocity_smooth', 'distance', 'grade_smooth', 'watts']
+streams = client.get_activity_streams(activity_id, types = types, resolution='medium')
 
-Fr = 0.004
-B = Fr * mass * g
+# for i in range(0, len(streams['time'].data)):
+#     print "{} {}".format(streams['distance'].data[i], streams['watts'].data[i])
 
-D = 0.5 * mass
+x = streams['distance'].data
+y = streams['altitude'].data
+inclin = streams['grade_smooth'].data
+section_dist = [1866.5, 3634.7, 5691.2, 7926.5, 9665.1, 11521.7, 14032.9]
 
-time = 0
+def get_time(section_power):
+    energy = 500000
+    velocities = []
+    mass = 90
+    g = 9.81
 
-energy = 1000
-#define energy properly
+    area = 0.32
+    Cd = 0.88
+    A = 0.5 * 1.225 * area * Cd
 
+    Fr = 0.004
+    B = Fr * mass * g
 
-def get_time(x, y, inclin, section_dist, section_power):
+    D = 0.5 * mass
     time = 0
-    for i in range(0, x.len() - 1):
-        theta = math.arctan(inclin[i]/100)
+    for i in range(len(x)-1):
+        theta = math.atan(inclin[i]/100)
         C = mass * g * math.sin(theta)
 
-        for j in range(0, section_dist.len()):
+        for j in range(0, len(section_dist)):
             if x[i] < section_dist[j]:
                 P = section_power[j]
                 break
 
 
-        coeff = [A, O, B + C, - P]
+        coeff = [A, 0, B + C, - P]
         v_array = np.roots(coeff)
 
         for n in v_array:
             if n.imag == 0:
-                velocities[i] = n
+                velocities.append(n)
                 break
 
         distance = math.sqrt((x[i + 1] - x[i])**2 + (y[i + 1] - y[i])**2)
@@ -61,4 +71,4 @@ def get_time(x, y, inclin, section_dist, section_power):
         if energy < 0:
             time += 1000000000
             break
-        return time
+    return time.real
