a4d2ec63877cc15b4bcdee030f7cbc0bc92a9c1e
RobPeacock <rob.peacock23@gmail.com>
2018-01-21 01:05:13 +0000

    Edit weather.py

--- weather.py
+++ weather.py
@@ -1,5 +1,7 @@
+import requests
 
+URL = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=c5914720e06793445e324ce52bab4f8c"
 
-STORED_ACCESS_TOKEN = "c5914720e06793445e324ce52bab4f8c"
+data = requests.get(URL).json()
 
-http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=STORED_ACCESS_TOKEN
+print data["wind"]
